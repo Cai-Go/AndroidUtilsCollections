7e52ef49ca50867dc4011ff939b7e66e735d48ab
Wu ZhaoHui <1158663782@qq.com>
2018-06-09 09:04:34 +0800

    ToastUtils

--- app/src/main/java/com/myandroid/wzh/androidutilscollections/MainActivity.java
+++ app/src/main/java/com/myandroid/wzh/androidutilscollections/MainActivity.java
@@ -3,11 +3,14 @@ package com.myandroid.wzh.androidutilscollections;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.myandroid.wzh.utilscollections.ToastUtil.ToastUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtils.toastShortShow(this,"sssssss");
     }
 }
